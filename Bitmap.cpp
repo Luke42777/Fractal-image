3a219ddf9739bc50c5d9d454ce1df0095d8db9ea
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-04 16:58:35 +0000

    Bitmap header values set

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -1,9 +1,18 @@
 #include "Bitmap.h"
+#include "BitmapInfoHeader.h"
+#include "BitmapFileHeader.h"
 
-Bitmap::Bitmap(int width, int height) : mWidth(width), mHeight(height), mptrPixels(new uint8_t[width * height * 3]){}
+Bitmap::Bitmap(int width, int height) : mWidth(width), mHeight(height), mptrPixels(new uint8_t[width * height * 3]{}) {}
 
 bool Bitmap::Write(std::string fileName)
 {
+	BitmapFileHeader fileHeader;
+	BitmapInfoHeader infoHeader;
+
+	fileHeader.fileSize = sizeof(fileHeader) + sizeof(infoHeader) + mWidth * mHeight * 3;//whole file size
+	fileHeader.dataOffSet = sizeof(fileHeader) + sizeof(infoHeader); // here our data starts
+	infoHeader.width = mWidth;
+	infoHeader.height = mHeight;
 
 	return true;
 
