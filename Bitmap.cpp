c363f747054f960e1c5d0ff0e00bbe361da24672
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-04 14:16:52 +0000

    constructor defined, Write() and SetPixel() at the moment declared only

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -1,3 +1,16 @@
 #include "Bitmap.h"
 
 Bitmap::Bitmap(int width, int height) : mWidth(width), mHeight(height) {}
+
+bool Bitmap::Write(std::string fileName)
+{
+
+	return true;
+
+}
+
+void Bitmap::SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
+{
+
+
+}
--- Bitmap.h
+++ Bitmap.h
@@ -1,9 +1,12 @@
 #pragma once
+#include <string>
+#include <cstdint>
 class Bitmap
 {
 public:
 	Bitmap(int width, int height);
-	bool write(); // after creating bitmap we want to write it
+	bool Write(std::string fileName); // after creating bitmap we want to write it
+	void SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
 
 
 
