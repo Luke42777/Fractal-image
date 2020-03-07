8a6ff6a269a44aa688d5ac11ea9bf3280ba5f95c
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-07 20:03:49 +0000

    Magnitude defined sbs function

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -6,9 +6,25 @@ using namespace  std;
 extern const int WIDTH;
 extern const int HEIGHT;
 
-static int GetIterations(int x, int y)
+ int Mandelbrot::GetIterations(int x, int y)
 {
 	complex <double> z = 0;
 	complex <double> c(x, y);
+
+	int iterations = 0;
+
+	//we will apply algorithm involving numbers above
+	//it's going to be different for each particular pixel
+	//and we are going to see how many loops we have to do before number starts to shoot to infinity
+	while (iterations < MAX_ITERATIONS)
+	{
+		z = z * z + c;
+		//there is also a notion of abolute value of complex numbers or (magnitude)
+		if (abs(z) > 2)
+		{
+			break;
+		}
+	}
+
 	return 0;
 }
