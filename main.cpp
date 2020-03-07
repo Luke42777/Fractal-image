0c60da983fafead039ccb91175a52f60f103b791
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-07 13:53:04 +0000

    Scaling and offset done

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main()
 			//the fractal algorithm would expect values from -1 to 1( not in the coordinate system of the bitmap
 			//but it expects x and y to range from minus 1 to 1 if we want to be able to see fractal
 			double xFractal = (x - WIDTH / 2.0) / (WIDTH / 2.0);
-			//double yFractal = y - HEIGHT / 2;
+			double yFractal = (y - HEIGHT / 2) / (HEIGHT / 2.0);
 
 			if (xFractal < min) { min = xFractal; }
 			//whenever we encounter a value of 'xFractal' to be less then current value
@@ -27,8 +27,8 @@ int main()
 			//and 'min' will end up being equal to the most minimum value of 'xFractal' that we encountered
 			if (xFractal > max) { max = xFractal; }
 
-			//if (yFractal < min) { min = yFractal; }
-			//if (yFractal > max) { max = yFractal; }
+			if (yFractal < min) { min = yFractal; }
+			if (yFractal > max) { max = yFractal; }
 		}
 		
 	}
