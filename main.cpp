4432d02a71b9c2d4020b90ce919db6ea00497a93
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-07 10:59:20 +0000

    Showing fractal started, scaling not finished

--- main.cpp
+++ main.cpp
@@ -8,14 +8,32 @@ const int HEIGHT = 600;
 
 int main()
 {
-	Bitmap bmap(WIDTH, HEIGHT);
+	Bitmap bmap(WIDTH, HEIGHT); 
+
+	double min = 999999;
+	double max = -999999;
 	for (int x = 0; x < WIDTH; x++)
 	{
 		for (int y = 0; y < HEIGHT; y++)
 		{
-			bmap.SetPixel(x, y, 255, 0, 0);
+			//the fractal algorithm would expect values from -1 to 1( not in the coordinate system of the bitmap
+			//but it expects x and y to range from minus 1 to 1 if we want to be able to see fractal
+			double xFractal = x - WIDTH / 2;
+			//double yFractal = y - HEIGHT / 2;
+
+			if (xFractal < min) { min = xFractal; }
+			//whenever we encounter a value of 'xFractal' to be less then current value
+			//we will set 'min' equal to that value
+			//and 'min' will end up being equal to the most minimum value of 'xFractal' that we encountered
+			if (xFractal > max) { max = xFractal; }
+
+			//if (yFractal < min) { min = yFractal; }
+			//if (yFractal > max) { max = yFractal; }
 		}
+		
 	}
+
+	cout << "min: " << min << ", max: " << max << endl;
 	bmap.Write("bitmap.bmp");
 	cout << "Finished";
 }
