73b396e41dc39b959dd792068b2ccf21ebe67c16
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-03-28 09:14:24 +0000

    Reorganization of main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include "Bitmap.h"
 #include <iostream>
-#include "Mandelbrot.h"
-#include <cstdint>
 #include <memory>
+#include <math.h> 
+#include <cstdint>
+#include "Bitmap.h"
+#include "Mandelbrot.h"
 
 
 using namespace std;
@@ -19,12 +20,13 @@ int main()
 	double min = 999999;
 	double max = -999999;
 
-	int* histogram = new int[Mandelbrot::MAX_ITERATIONS]{};
-	//this will be our color histogram
-	//we need to know how many times certain iterations occours
-	//at the beginning is set to Zero
+	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]);
+	unique_ptr<int[]> ptr_fractal(new int[WIDTH * HEIGHT]);//this array will store informations about how many iterations has every single pixel
 
-	int* ptr_Fractal = new int[HEIGHT*WIDTH]{};//this array will store informations about how many iterations has every single pixel
+	// fill arrays with 0
+	fill(histogram.get(), histogram.get() + Mandelbrot::MAX_ITERATIONS, 0);
+	fill(ptr_fractal.get(), ptr_fractal.get() + WIDTH * HEIGHT, 0);
+
 
 	for (int y = 0; y < HEIGHT; y++) //this first nested loop simply store information we need
 	{
@@ -32,16 +34,17 @@ int main()
 		{
 			//the fractal algorithm would expect values from -1 to 1( not in the coordinate system of the bitmap
 			//but it expects x and y to range from minus 1 to 1 if we want to be able to see fractal
-			double xFractal = (x - WIDTH / 2 - 150) / (HEIGHT / 2.0) ;
+			double xFractal = (x - WIDTH / 2 - 200) / (HEIGHT / 2.0) ;
 			double yFractal = (y - HEIGHT / 2) / (HEIGHT / 2.0);
 
 			int iterations = Mandelbrot::GetIterations(xFractal, yFractal);
-			if (iterations < Mandelbrot::MAX_ITERATIONS)
+			ptr_fractal[y * WIDTH + x] = iterations;
+			if (iterations != Mandelbrot::MAX_ITERATIONS)
 			{
 				histogram[iterations]++;
 			}
 
-			ptr_Fractal[y*WIDTH +x] = iterations;	
+			
 		}	
 	}
 	int total = 0; // to improve image we need to have total number of iterations
@@ -54,19 +57,24 @@ int main()
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
-			int iterations = ptr_Fractal[y * WIDTH + x];
-	
-			double hue = 0.0; // this should be between zero and 1
-
-			for (int i = 0; i <= iterations; i++)
-			{
-				hue += ((double)histogram[i]) / total;
-			}
-
 			uint8_t red = 0;
-			uint8_t green = hue * 255;
+			uint8_t green = 0;
 			uint8_t blue = 0;
 
+			int iterations = ptr_fractal[y * WIDTH + x];
+			
+			if (iterations != Mandelbrot::MAX_ITERATIONS)
+			{
+				double hue = 0.0; // this should be between zero and 1
+
+					for (int i = 0; i <= iterations; i++)
+					{
+						hue += ((double)histogram[i]) / total;
+					}
+
+				 green = pow(255, hue);
+
+			}
 
 			bmap.SetPixel(x, y, red, green, blue);
 
@@ -75,5 +83,5 @@ int main()
 	
 	bmap.Write("bitmap.bmp");
 	cout << "Finished";
-	delete[] histogram;
+	
 }
